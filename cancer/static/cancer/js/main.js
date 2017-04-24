354f8391a1002d3793eb64a05891c35ff140321d
palak12345 <you@example.com>
2017-04-24 12:22:07 +0530

    Initialized project

---
+++ cancer/static/cancer/js/main.js
@@ -0,0 +1,60 @@
+var AccessListPage = {
+	init: function() {
+		this.$container = $('.access-container');
+		this.render();
+		this.bindEvents();
+	},
+
+	render: function() {
+
+	},
+
+	bindEvents: function() {
+		$('.btn-favorite', this.$container).on('click', function(e) {
+			e.preventDefault();
+
+			var self = $(this);
+			var url = $(this).attr('href');
+			$.getJSON(url, function(result) {
+				if (result.success) {
+					$('.glyphicon-star', self).toggleClass('active');
+				}
+			});
+
+			return false;
+		});
+	}
+};
+
+var QueryListPage = {
+	init: function() {
+		this.$container = $('.Query-container');
+		this.render();
+		this.bindEvents();
+	},
+
+	render: function() {
+
+	},
+
+	bindEvents: function() {
+		$('.btn-favorite', this.$container).on('click', function(e) {
+			e.preventDefault();
+
+			var self = $(this);
+			var url = $(this).attr('href');
+			$.getJSON(url, function(result) {
+				if (result.success) {
+					$('.glyphicon-star', self).toggleClass('active');
+				}
+			});
+
+			return false;
+		});
+	}
+};
+
+$(document).ready(function() {
+	AccessListPage.init();
+	QueryListPage.init();
+});
